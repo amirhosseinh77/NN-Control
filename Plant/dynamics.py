5e83b746c59e87fdc708d09e43de4cde6eb38a0f
Amirhossein Heydarian <amirhossein261077@live.com>
2024-01-24 14:42:42 +0330

    added some base code

--- Plant/dynamics.py
+++ Plant/dynamics.py
@@ -10,3 +10,16 @@ class SystemDynamics:
 
   def outputEqn(self):
     pass
+
+
+class MultiAgent:
+  def __init__(self, number, sample_time=0.1):
+    self.dt = sample_time
+    self.agents = np.random.randint(0,100,(number,2)).astype('float')
+    self.agents = np.hstack([self.agents, np.zeros((number,2))])
+
+  def update_state(self, u):
+    q_dot = u
+    self.agents[:,2:] += q_dot * self.dt
+    p_dot = self.agents[:,2:]
+    self.agents[:,:2] += p_dot * self.dt
--- Plant/env.py
+++ Plant/env.py
@@ -1,1 +1,23 @@
+import gymnasium
+from gymnasium import spaces
+import numpy as np
+
+
+class PlantEnv(gymnasium.Env):
+    def __init__(self):
+        super(PlantEnv, self).__init__()
+        self.dynamics = \\
+        self.action_space = spaces.Box(low=0, high=np.ones(), shape=(,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(,), dtype=np.float32) # HEIGHT, WIDTH, N_CHANNELS
+
+    def step(self, action):
+        return next_state.astype(np.float32), reward, done, False, info
+
+    def reset(self, seed=None, options={}):
+        return state.ravel().astype(np.float32), info
+
+    def render(self):
+        pass
+
+
 
